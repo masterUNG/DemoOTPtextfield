a545380112bf19795a92db10773b34e16400df3d
masterUNG <phrombutr@gmail.com>
2021-05-26 09:25:44 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_app_web/Service/Auth_Service.dart';
+import 'package:firebase_app_web/pages/HomePage.dart';
+import 'package:firebase_app_web/pages/SignInPage.dart';
+import 'package:firebase_app_web/pages/SignUpPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget currentPage = SignUpPage();
+  AuthClass authClass = AuthClass();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkLogin();
+  }
+
+  void checkLogin() async {
+    String token = await authClass.getToken();
+    if (token != null) {
+      setState(() {
+        currentPage = HomePage();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: currentPage,
+    );
+  }
+}
